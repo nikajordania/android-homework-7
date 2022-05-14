b2f6896ee9cce433eab9a94f8a62434e2d88a61c
Nika Zhordania <nika.zhordania.1@btu.edu.ge>
2022-05-14 23:55:58 +0400

    add: UI

--- app/src/main/java/com/example/androidhomework7/MainActivity.kt
+++ app/src/main/java/com/example/androidhomework7/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidhomework7.api.dto.RestClient
@@ -22,8 +23,15 @@ class MainActivity : AppCompatActivity() {
 
         RestClient.initClient()
 
+        val name = findViewById<EditText>(R.id.name)
+        val job = findViewById<EditText>(R.id.job)
+        val id = findViewById<EditText>(R.id.id)
+
         val listUsersBtn = findViewById<Button>(R.id.listUsers)
+        val createUserBtn = findViewById<Button>(R.id.createUser)
+        val updateUserBtn = findViewById<Button>(R.id.updateUser)
         val deleteUserBtn = findViewById<Button>(R.id.deleteUser)
+
         val result = findViewById<TextView>(R.id.result)
 
         listUsersBtn.setOnClickListener {
@@ -35,43 +43,28 @@ class MainActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         Log.d("Successful", response.body().toString())
                         result.text = response.body().toString()
-//                response.body()?.data?.forEach { user -> println("nika $user") }
                     }
                 }
 
                 override fun onFailure(call: Call<ListUsers>, t: Throwable) {
                     Log.d("Failure", t.message.toString())
                 }
-
             })
         }
 
-        deleteUserBtn.setOnClickListener {
-            RestClient.reqResService.deleteUser(2).enqueue(object : Callback<okhttp3.ResponseBody> {
-                @SuppressLint("SetTextI18n")
-                override fun onResponse(
-                    call: Call<okhttp3.ResponseBody>,
-                    response: Response<okhttp3.ResponseBody>
-                ) {
-                    if (response.isSuccessful) {
-                        Log.d("Successful", "${response.code()} :Success Deleted")
-                        result.text = "${response.code()} :Success Deleted"
-                    }
-                }
-
-                override fun onFailure(call: Call<okhttp3.ResponseBody>, t: Throwable) {
-                    Log.d("Failure", t.message.toString())
-                }
-
-            })
-        }
-        RestClient.reqResService.createUser(UserRequest("nika", "QA"))
-            .enqueue(object : Callback<CreateUserResponse> {
+        createUserBtn.setOnClickListener {
+            RestClient.reqResService.createUser(
+                UserRequest(
+                    name = name.text.toString(),
+                    job = job.text.toString()
+                )
+            ).enqueue(object : Callback<CreateUserResponse> {
                 override fun onResponse(
                     call: Call<CreateUserResponse>, response: Response<CreateUserResponse>
                 ) {
                     if (response.isSuccessful) {
                         Log.d("Successful", response.body().toString())
+                        result.text = response.body().toString()
 //                response.body()?.data?.forEach { user -> println("nika $user") }
                     }
                 }
@@ -79,16 +72,22 @@ class MainActivity : AppCompatActivity() {
                 override fun onFailure(call: Call<CreateUserResponse>, t: Throwable) {
                     Log.d("Failure", t.message.toString())
                 }
-
             })
+        }
 
-        RestClient.reqResService.updateUser(UserRequest("nika", "QA"), 2)
-            .enqueue(object : Callback<UpdateUserResponse> {
+        updateUserBtn.setOnClickListener {
+            RestClient.reqResService.updateUser(
+                UserRequest(
+                    name = name.text.toString(),
+                    job = job.text.toString()
+                ), id = id.text.toString().toLong()
+            ).enqueue(object : Callback<UpdateUserResponse> {
                 override fun onResponse(
                     call: Call<UpdateUserResponse>, response: Response<UpdateUserResponse>
                 ) {
                     if (response.isSuccessful) {
                         Log.d("Successful", response.body().toString())
+                        result.text = response.body().toString()
 //                response.body()?.data?.forEach { user -> println("nika $user") }
                     }
                 }
@@ -96,10 +95,27 @@ class MainActivity : AppCompatActivity() {
                 override fun onFailure(call: Call<UpdateUserResponse>, t: Throwable) {
                     Log.d("Failure", t.message.toString())
                 }
-
             })
+        }
 
+        deleteUserBtn.setOnClickListener {
+            RestClient.reqResService.deleteUser(id.text.toString().toLong())
+                .enqueue(object : Callback<okhttp3.ResponseBody> {
+                    @SuppressLint("SetTextI18n")
+                    override fun onResponse(
+                        call: Call<okhttp3.ResponseBody>,
+                        response: Response<okhttp3.ResponseBody>
+                    ) {
+                        if (response.isSuccessful) {
+                            Log.d("Successful", "${response.code()} :Success Deleted")
+                            result.text = "${response.code()} :Success Deleted"
+                        }
+                    }
 
+                    override fun onFailure(call: Call<okhttp3.ResponseBody>, t: Throwable) {
+                        Log.d("Failure", t.message.toString())
+                    }
+                })
+        }
     }
-
 }
